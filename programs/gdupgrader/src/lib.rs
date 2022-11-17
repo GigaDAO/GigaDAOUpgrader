2fefd8326af33ea62fc02ee663a6b51247b431e7
alphaprime8 <alphaprime8888@gmail.com>
2022-11-17 18:53:42 +0800

    built instruction

--- programs/gdupgrader/src/lib.rs
+++ programs/gdupgrader/src/lib.rs
@@ -28,10 +28,8 @@ pub mod gdupgrader {
         let seeds = &[&MULTISIG_PDA_SEED[..], &[bump_seed]];
         let signer = &[&seeds[..]];
 
-        // // TODO add accounts below to context
-        //
-        // // TODO replace with account info from fed in accounts
         // let bfp_program_upgrade_address = bpf_loader_upgradeable::id();
+        // let target_program_address = &Pubkey::from_str("7w9oX4fSFFW9YK7iWYqBUzEwXJHa3UY3wP4y8HvpaU2s")?;
         // let buffer_address = &Pubkey::from_str("3rkWkQ1dzhVgdUSWqscBQqzBpB6nnzppbnnFaHPVuNwG")?;
         //
         // let upgrade_ix = program_upgrade(
@@ -40,43 +38,32 @@ pub mod gdupgrader {
         //     pda_authority_address,
         //     spill_address,
         // );
-        //
-        // let accounts_iter = &mut accounts.iter();
-        //
-        // let signer_account = next_account_info(accounts_iter)?;
-        // let multisig_account = next_account_info(accounts_iter)?;
-        // let target_program = next_account_info(accounts_iter)?;
-        // let program_buffer = next_account_info(accounts_iter)?;
-        // let source_buffer = next_account_info(accounts_iter)?;
-        // let rent_program = next_account_info(accounts_iter)?;
-        // let clock_program = next_account_info(accounts_iter)?;
-        //
-        // let current_authority = multisig_account.key;
-        //
-        // let instruction = Instruction::new_with_bincode(
-        //     bpf_loader_upgradeable::id(),
-        //     &UpgradeableLoaderInstruction::Upgrade,
-        //     vec![
-        //         AccountMeta::new(*program_buffer.key, false),
-        //         AccountMeta::new(*target_program.key, false),
-        //         AccountMeta::new(*source_buffer.key, false),
-        //         AccountMeta::new(*signer_account.key, false),
-        //         AccountMeta::new_readonly(sysvar::rent::id(), false),
-        //         AccountMeta::new_readonly(sysvar::clock::id(), false),
-        //         AccountMeta::new_readonly(*current_authority, true),
-        //     ],
-        // );
+
+        let instruction = Instruction::new_with_bincode(
+            bpf_loader_upgradeable::id(),
+            &UpgradeableLoaderInstruction::Upgrade,
+            vec![
+                AccountMeta::new(ctx.accounts.target_program_buffer.key(), false), // target program buffer
+                AccountMeta::new(ctx.accounts.target_program.key(), false), // target program
+                AccountMeta::new(ctx.accounts.source_buffer.key(), false), // tmp buffer account
+                AccountMeta::new(ctx.accounts.signer.key(), false), // spill account
+                AccountMeta::new_readonly(sysvar::rent::id(), false),
+                AccountMeta::new_readonly(sysvar::clock::id(), false),
+                AccountMeta::new_readonly(ctx.accounts.multisig_pda.key(), true), // multisig PDA
+            ],
+        );
+
         //
         // invoke_signed(
         //     &instruction,
         //     &[
-        //         program_buffer.clone(),
-        //         target_program.clone(),
-        //         source_buffer.clone(),
-        //         signer_account.clone(), // used as spill account
+        //         program_buffer.clone(), // target program buffer
+        //         target_program.clone(), // target program
+        //         source_buffer.clone(), // tmp buffer account
+        //         signer_account.clone(), // spill account (signer)
         //         rent_program.clone(),
         //         clock_program.clone(),
-        //         multisig_account.clone(),
+        //         multisig_account.clone(), // multisig PDA
         //     ],
         //     &[&[b"multisig", target_program.key.as_ref(), &[multisig_bump]]],
         // )?;
@@ -88,8 +75,18 @@ pub mod gdupgrader {
 #[derive(Accounts)]
 pub struct Upgrade<'info> {
     #[account(mut)]
+    /// CHECK: bypass
+    pub target_program_buffer: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK: bypass
+    pub target_program: AccountInfo<'info>,
+    #[account(mut)]
+    /// CHECK: bypass
+    pub source_buffer: AccountInfo<'info>,
+    #[account(mut)]
     pub signer: Signer<'info>,
-
+    pub rent: Sysvar<'info, Rent>,
+    pub clock: Sysvar<'info, Clock>,
     #[account(
     init_if_needed,
     seeds = [MULTISIG_PDA_SEED],
@@ -98,9 +95,7 @@ pub struct Upgrade<'info> {
     space = MIN_ACCOUNT_LEN,
     )]
     pub multisig_pda: Account<'info, AuthAccount>,
-
     pub system_program: Program<'info, System>,
-    pub rent: Sysvar<'info, Rent>,
 }
 
 #[account]
